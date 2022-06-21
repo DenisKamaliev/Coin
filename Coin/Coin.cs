ff9b8f7d9a53baa2123ea372de1ea2eafc3e179b
Denis <Kds04042002omgu@gmail.com>
2022-06-22 01:44:51 +0600

    Добавьте файлы проекта.

---
+++ Coin/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin
+{
+    internal class Coin
+    {
+        public static bool Flip(int Player)
+        {
+            int Random = new Random().Next(2);
+            if (Player == Random)
+                return true;
+            return false;
+        }
+       
+    }
+}
---
+++ Coin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int counter = 0;
+        int win = 0;
+        Console.WriteLine("Игра началась!");
+        string input = Console.ReadLine();
+        int player = int.Parse(input);
+        if ((player == 0 || player == 1 )&& Coin.Coin.Flip(player))
+        {
+            counter++;
+            win++;
+        }
+
+        else if (player == 0 || player == 1)
+            counter++;
+        while (player == 0 || player == 1)
+        {
+            Console.WriteLine("Введите число: ");
+            input = Console.ReadLine();
+            player = int.Parse(input);
+            if ((player == 0 || player == 1) && Coin.Coin.Flip(player))
+            {
+                counter++;
+                win++;
+            }
+            else if (player == 0 || player == 1)
+                counter++;
+        }
+        float result = win*100 / counter;
+        Console.WriteLine("Игра окончена со счётом " + win + ", угадано " + result + "% бросков");
+
+    }
+}
